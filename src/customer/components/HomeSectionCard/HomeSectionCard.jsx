0b14b07617fc7cfcf1d894926d468fa538b08380
Abhishek Sengar <abhisheksengar04042k1@gmail.com>
2024-01-03 04:21:31 +0530

    Home section product carousel

--- src/customer/components/HomeSectionCard/HomeSectionCard.jsx
+++ src/customer/components/HomeSectionCard/HomeSectionCard.jsx
@@ -1,22 +1,21 @@
-/* eslint-disable jsx-a11y/alt-text */
-import React from 'react'
+import React from 'react';
 
-const HomeSectionCard = () => {
+const HomeSectionCard = ({ product }) => {
   return (
-    <div className='cursor-pointer flex flex-col items-center bg-white rounded-lg shadow-lg overflow-hidden w-[15rem] mx-3'>
-        <div className='h-[13rem] w-[10rem]'>
-            <img 
-                className='object cover object-top w-full h-full'
-                src='https://m.media-amazon.com/images/I/61CX-+v3NwL._AC_UL480_FMwebp_QL65_.jpg'
-                alt='White Kurta'
-            />
-        </div>
-        <div className='p-4'>
-            <h3 className='text-lg font-medium text-gray-900'>Peter England</h3>
-            <p className='mt-2 text-sm text-gray-500'>Men's Cotton Blend Slim Tunic Kurta</p>
-        </div>
+    <div className='cursor-pointer flex flex-col items-center bg-white rounded-lg shadow-lg overflow-hidden w-[15rem] mx-3 border'>
+      <div className='h-[13rem] w-[10rem]'>
+        <img
+          className='object-cover object-top w-full h-full'
+          src={product.imageUrl}
+          alt='White Kurta'
+        />
+      </div>
+      <div className='p-4'>
+        <h3 className='text-lg font-medium text-gray-900'>{product.brand}</h3>
+        <p className='mt-2 text-sm text-gray-500'>{product.title}</p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HomeSectionCard
+export default HomeSectionCard;
--- src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
+++ src/customer/components/HomeSectionCarousel/HomeSectionCarousel.jsx
@@ -1,34 +1,69 @@
-import React from 'react'
+import React from 'react';
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
 import HomeSectionCard from '../HomeSectionCard/HomeSectionCard';
-import { Button } from '@mui/material'
+import { Button } from '@mui/material';
 import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
-
-const responsive = {
-    0: { items: 1 },
-    720: { items: 3 },
-    1024: { items: 5.5 },
-};
-
-const items = [1, 1, 1, 1, 1].map((item) => <HomeSectionCard />)
+import { mens_kurta } from '../../../Data/mens_kurta';
 
 const HomeSectionCarousel = () => {
+    const responsive = {
+        0: { items: 1 },
+        720: { items: 3 },
+        1024: { items: 5.5 },
+    };
+
+    const items = mens_kurta.slice(0, 10).map((item, index) => (
+        <HomeSectionCard product={item} key={index} />
+    ));
+
     return (
-        <div className='relative px-4 lg:px-8'>
+        <div className='border'>
             <div className='relative p-5'>
                 <AliceCarousel
                     items={items}
-                    disableButtonsControls
-                    infinite
                     responsive={responsive}
+                    disableDotsControls
+                    renderPrevButton={({ isDisabled, onClick }) => (
+                        <Button
+                            onClick={onClick}
+                            disabled={isDisabled}
+                            variant='contained'
+                            className='z-50 bg-white'
+                            sx={{
+                                position: 'absolute',
+                                top: '8rem',
+                                left: '0rem',
+                                transform: 'translateX(-50%) rotate(-90deg)',
+                                bgcolor: 'white',
+                            }}
+                            aria-label='prev'
+                        >
+                            <KeyboardArrowLeftIcon sx={{ transform: 'rotate(90deg)', color: 'black' }} />
+                        </Button>
+                    )}
+                    renderNextButton={({ isDisabled, onClick }) => (
+                        <Button
+                            onClick={onClick}
+                            disabled={isDisabled}
+                            variant='contained'
+                            className='z-50 bg-white'
+                            sx={{
+                                position: 'absolute',
+                                top: '8rem',
+                                right: '0rem',
+                                transform: 'translateX(50%) rotate(90deg)',
+                                bgcolor: 'white',
+                            }}
+                            aria-label='next'
+                        >
+                            <KeyboardArrowLeftIcon sx={{ transform: 'rotate(90deg)', color: 'black' }} />
+                        </Button>
+                    )}
                 />
-                <Button variant='contained' className='z-50' sx={{position : 'absolute', top : "8rem", right : "0rem", transform : "translateX(50%) rotate(90deg)"}} aria-label='next'>
-                    <KeyboardArrowLeftIcon />
-                </Button>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default HomeSectionCarousel
+export default HomeSectionCarousel;
