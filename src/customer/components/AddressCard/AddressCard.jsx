41c1978e75cbb65579464e12d721371da99d5777
Abhishek Sengar <abhisheksengar04042k1@gmail.com>
2024-02-18 22:10:48 +0530

    Address and Order summary UI

---
+++ src/customer/components/AddressCard/AddressCard.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const AddressCard = () => {
+  return (
+    <div>
+      <div className='space-y-3'>
+        <p className='font-semibold'>Abhishek Sengar</p>
+        <p>Madhya Pradesh, Gwalior, Radhapuram, 474020</p>
+        <div className='space-y-1'>
+            <p className='font-semibold'>Phone Number</p>
+            <p>9176543214</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default AddressCard
